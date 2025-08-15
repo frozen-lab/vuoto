06e776078453c2eee3fef313b8d50c067a9e1364
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-08-15 21:13:36 +0530

    (cli) cache init

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+use turbocache::TurboCache;
+
+const HOME_DIR: &str = "vuoto_cli";
+
 fn main() {
-    println!("Vuoto");
+    let home_dir = get_app_dir();
+
+    // exit w/ error
+    if home_dir.is_none() {
+        eprintln!("Unable to read HOMEDIR");
+        std::process::exit(1);
+    }
+
+    let cache_path = home_dir.unwrap().join("vault");
+    let _cache = TurboCache::new(cache_path, 512).expect("Cache init");
+}
+
+fn get_app_dir() -> Option<PathBuf> {
+    if let Some(path) = env_home::env_home_dir() {
+        return Some(path.join(&HOME_DIR));
+    }
+
+    None
 }
