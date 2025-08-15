501af1437d58b8cc41fc9d0c5b0b656a00a9e454
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-08-15 19:11:27 +0530

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          rust = pkgs.mkShell {
+            name = "dev-rust";
+            buildInputs = with pkgs; [
+              gcc
+              pkg-config
+              rustc
+              cargo
+              rustfmt
+              clippy
+              rust-analyzer
+            ];
+
+            shellHook = ''
+              export RUST_BACKTRACE=1
+              
+              echo " : $(rustc --version | cut -d' ' -f2)"
+            '';
+          };
+        };
+      }
+    );
+}
