dec015190d452763e0c75cb785f5b91c792e3be5
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-08-16 13:23:38 +0530

    (cli) vault creation and selection

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,7 +3,11 @@
 mod types;
 mod vaults;
 
-use crate::types::{InternalError, InternalResult};
+use crate::{
+    types::{InternalError, InternalResult},
+    vaults::VaultIndex,
+};
+use inquire::{Select, Text};
 use std::path::PathBuf;
 use turbocache::TurboCache;
 
@@ -13,8 +17,54 @@ fn main() -> InternalResult<()> {
     let home_dir = get_app_dir()?;
     let cache_path = home_dir.join("vault");
     let _cache = TurboCache::new(cache_path, 512)?;
+    let mut vault_idx = VaultIndex::open(&home_dir)?;
 
+    let vault = loop {
+        let mut options = vault_idx.vaults().to_vec();
+
+        // no vaults yet => force creation
+        if options.is_empty() {
+            let new_vault = prompt_new_vault()?;
+            vault_idx.add(&new_vault)?;
+
+            break new_vault;
+        } else {
+            // add special option
+            options.push("< Create new vault >".into());
+
+            let ans = Select::new("Your Vaults:", options).prompt();
+
+            match ans {
+                Ok(choice) => {
+                    if choice == "< Create new vault >" {
+                        let new_vault = prompt_new_vault()?;
+                        vault_idx.add(&new_vault)?;
+
+                        continue;
+                    } else {
+                        break choice;
+                    }
+                }
+                Err(err) => return_error(format!("{err}")),
+            }
+        }
+    };
+
+    println!("Vault selected: {}", vault);
     Ok(())
+}
+
+fn prompt_new_vault() -> InternalResult<String> {
+    let ans = Text::new("Enter name for new vault:")
+        .prompt()
+        .map_err(|e| InternalError::IO(format!("Failed to read input: {e}")))?;
+
+    Ok(ans)
+}
+
+fn return_error(msg: String) -> ! {
+    eprintln!("[ERROR]: {msg}");
+    std::process::exit(1);
 }
 
 pub(crate) fn get_app_dir() -> InternalResult<PathBuf> {
